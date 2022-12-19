dae21dd20deb5f1113dd4e19861185002e8ec0e8
Inozpavel <inozpavel@mail.ru>
2022-12-19 16:38:37 +0300

    Add unit tests

---
+++ WeatherMicroservice.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using WeatherMicroservice.Controllers;
+
+namespace WeatherMicroservice.Tests;
+
+[TestFixture]
+public class WeatherForecastControllerTests
+{
+    [Test]
+    [TestCase("Name")]
+    [TestCase("Name with space")]
+    [TestCase("123")]
+    public void CanFetForecastForUser(string username)
+    {
+        var controller = new WeatherForecastController();
+
+        var result = controller.GetForPerson(username);
+
+        Assert.True(result.Forecast.Contains(username));
+    }
+}
